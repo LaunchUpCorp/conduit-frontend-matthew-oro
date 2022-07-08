cd78739d1d191e80471e191bed94ef73a230967c
Matthew Oro <MatthewEOro@gmail.com>
2022-07-07 21:48:53 -0400

    feat: Create header navigation

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,11 +1,13 @@
 import Banner from './Banner'
 import Feed from './Feed'
 import Article from './Article'
+import Header from './Header'
 
 
 function HomePage(){
   return(
     <div>
+      <Header/>
       <Banner/>
       <Feed/>
       <Article/>
