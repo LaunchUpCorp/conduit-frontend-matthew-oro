99c9422e4b43692980cec3003ba212b91d50043e
Matthew Oro <MatthewEOro@gmail.com>
2022-07-09 11:09:21 -0400

    refactor: Move "Feed" component into "Banner" component.

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,5 +1,4 @@
 import Banner from './Banner'
-import Feed from './Feed'
 import Header from './Header'
 
 function HomePage(){
@@ -7,7 +6,6 @@ function HomePage(){
     <div>
       <Header/>
       <Banner/>
-      <Feed/>
     </div>
   )
 }
