d84e212db027f6a5a93f7849f58867db9ca8ed49
Matthew Oro <MatthewEOro@gmail.com>
2022-07-09 15:35:55 -0400

    refactor: move "Feed" and "PopularTags" components out of "Banner" component and into "HomePage" component.

--- src/components/Homepage/HomePage.js
+++ src/components/Homepage/HomePage.js
@@ -1,11 +1,17 @@
 import Banner from './Banner'
 import Header from './Header'
+import Feed from "./Feed";
+import PopularTags from "./PopularTags";
 
 function HomePage(){
   return(
     <div>
       <Header/>
       <Banner/>
+      <div className="flex flex-col-reverse px-[25%] md:flex-row md:items-cstart md:justify-center ">
+        <Feed />
+        <PopularTags />
+      </div>
     </div>
   );
 }
